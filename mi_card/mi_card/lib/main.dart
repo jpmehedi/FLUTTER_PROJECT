61de9969d8c9f89dd909bcfabe4a13db49e4a0da
DESKTOP-0B563Q2\engrm <engrmhnirob47@gmail.com>
2020-01-11 23:59:45 +0600

    complete design mi_card

--- mi_card/mi_card/lib/main.dart
+++ mi_card/mi_card/lib/main.dart
@@ -2,15 +2,84 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
-  @override 
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("MI CARD"),),
-        body: Text("Hello World"),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.00,
+                backgroundImage: AssetImage('images/avatar.png'),
+              ),
+              Text(
+                'Mis. Nafisa',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 45.00,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Flutter Developer'.toUpperCase(),
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.00,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              SizedBox(
+                height: 10.00,
+                width: 150.00,
+                child: Divider(
+                  color:Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.00,horizontal: 25.00),
+                child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+880-1790180825',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.00,horizontal: 25.00),
+                  child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'tech4webs@gmail.com',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+              ),
+            ],
+          ),
+        ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
-
 }
